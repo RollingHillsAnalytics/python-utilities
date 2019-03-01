ab7eb170af9db2058b259ec0e74e717530891a41
RollingHillsAnalytics <47510980+RollingHillsAnalytics@users.noreply.github.com>
2019-03-01 12:59:04 -0800

    Update python_utilities.py

--- python_utilities.py
+++ python_utilities.py
@@ -1,7 +1,8 @@
-# The DisplayDuration function is used to correctly display the completion
-# time of a function or program in hours, minutes and seconds, including
-# the correct spelling of 'hour(s)', 'minute(s)' and 'second(s)'.
+# The DisplayDuration function is used to correctly display the completion time of a function or program 
+# in hours, minutes and seconds, including the correct spelling of 'hour(s)', 'minute(s)' and 'second(s)'.
+#
 # The output is "Completed in xx hour xx minutes xx seconds."
+
 def DisplayDuration(starttime, endtime):
 
 	m, s = divmod(int(endtime - starttime), 60)
